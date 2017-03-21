ae8737f0500cfbd9e092c490b33a4f33c51298ba
Mr. Super <linzechao44@163.com>
2017-03-22 00:26:50 +0800

    完善笔记

--- framework/vue/first/src/App.vue
+++ framework/vue/first/src/App.vue
@@ -16,18 +16,12 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style sass>
+    #app {
+        background-color: #369;
 
-}
-
-    img {
-        width: 100%;
+        img {
+            width: 200px;
+        }
     }
 </style>
