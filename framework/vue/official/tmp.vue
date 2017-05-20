2531289819bcbf5b6b93b5ba677795a1abf57f2d
MR.Super <linzechao44@163.com>
2017-05-20 16:16:10 +0800

    [DELETE]删除不必要的旧框架

---
+++ framework/vue/official/tmp.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+            return {
+                greeting: 'Hello'
+            }
+        }
+    };
+</script>
+
+<style>
+p {
+    font-size: 14px;
+    text-align: center;
+}
+</style>
